67520c5e6878fce6f52d5efc17cd18075bab26b0
Jack DeVries <jdevries3133@gmail.com>
2022-05-08 11:35:47 -0400

    remove redundant comments

--- variables.tf
+++ variables.tf
@@ -8,18 +8,11 @@ variable "domain" {
   description = "Public domain name for the application. App pods will be exposed via a service and ingress."
 }
 
-/**
- * Used for namespace, and as a prefix for services, pods, etc.
- */
 variable "app_name" {
   type        = string
   description = "Name of the application. Used as the namespace, and used as a prefix for other objects. For example, \"appname-service\""
 }
 
-/**
- * Application container port that should ultimately be exposed to the outside
- * world.
- */
 variable "application_port" {
   type        = number
   default     = 8000
